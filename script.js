74a1d4d36ed6f8c6ceba152fa89cee99c38234d2
Renars Robergs <rencha1102@gmail.com>
2019-05-14 14:03:05 +0300

    1st

---
+++ script.js
@@ -0,0 +1,40 @@
+var g = 0;
+
+function Random() {
+  var g = Math.floor(Math.random() * 10);
+  
+
+  if (g == 0) {
+    console.log("💀").value = g;
+  }
+  if (g == 1) {
+    console.log("💩").value = g;
+  }
+  if (g == 2) {
+    console.log("😭").value = g;
+  }
+  if (g == 3) {
+    console.log("😢").value = g;
+  }
+  if (g == 4) {
+    console.log("😓").value = g;
+  }
+  if (g == 5) {
+    console.log("😕").value = g;
+  }
+  if (g == 6) {
+    console.log("🙃").value = g;
+  }
+  if (g == 7) {
+    console.log("🤔").value = g;
+  }
+  if (g == 8) {
+    console.log("🧐").value = g;
+  }
+  if (g == 9) {
+    console.log("🤑").value = g;
+  }
+  if (g == 10) {
+    console.log("🤯").value = g;
+  }
+}
